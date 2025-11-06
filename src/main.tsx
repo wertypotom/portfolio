a23c3544ae063a67d9220b365b14a28ee12d7fa4
Andrey Povstyanko <andrey.povstyanko@m-files.com>
2025-11-06 15:49:59 -0600

    fixed analyztics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import { Analytics } from '@vercel/analytics/next'
+import { Analytics } from '@vercel/analytics/react'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './main.css'
